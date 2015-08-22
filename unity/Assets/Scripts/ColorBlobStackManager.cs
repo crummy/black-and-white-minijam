657e07492ddafffcfc027e9a05fd75f84cbf524b
Skiri-ki <git@skiri-ki.com>
2015-08-22 18:00:56 +0200

    Black And White Blob Spawning

--- unity/Assets/Scripts/ColorBlobStackManager.cs
+++ unity/Assets/Scripts/ColorBlobStackManager.cs
@@ -8,13 +8,42 @@ using System.Collections.Generic;
 [RequireComponent(typeof(Camera))]
 public class ColorBlobStackManager : Singleton<ColorBlobStackManager> {
 	
+	public SpericalColorBlob blackSpherePrefab;
+	public SpericalColorBlob whiteSpherePrefab;
+	
 	private Stack<ColorBlob> colorStack;
 	private Camera renderTextureCamera;
+	
+	private const float DistanceBetweenLayers = 0.01f;
 	
 	public void PushColorBlob(ColorBlob blob)
 	{
 		colorStack.Push(blob);
+		transform.position = transform.position + Vector3.up * DistanceBetweenLayers;
+		renderTextureCamera.farClipPlane += DistanceBetweenLayers;
 	}
+	
+	public void SpawnSpericalColorBlob(ColorBlob.BlobColor color, Vector2 position, float radius = 1.0f)
+	{
+		SpericalColorBlob spawnedBlob = null;
+		switch (color) {
+		case ColorBlob.BlobColor.Black:
+			spawnedBlob = Instantiate(blackSpherePrefab);
+			break;
+		case ColorBlob.BlobColor.White:
+			spawnedBlob = Instantiate(whiteSpherePrefab);
+			break;
+		default:
+			break;
+		}
+		
+		spawnedBlob.Radius = radius;
+		
+		spawnedBlob.transform.position = new Vector3(position.x, transform.position.y + 1, position.y);
+		
+		PushColorBlob(spawnedBlob);
+	}
+	
 	
 	protected override void Awake ()
 	{
---
+++ unity/Assets/Scripts/SpericalColorBlob.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpericalColorBlob : ColorBlob {
+	private float radius;
+	public float Radius
+	{
+		get
+		{
+			return radius;
+		}
+		set
+		{
+			transform.localScale = new Vector3(value, value, value);
+			radius = value;
+		}
+	}
+}
