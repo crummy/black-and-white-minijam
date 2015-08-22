7416701ab56660fd900358e8168fcee9dfb0a371
Malcolm <crummynz@gmail.com>
2015-08-22 18:18:45 +0200

    Begun adding RocketController

---
+++ unity/Assets/Scripts/RocketController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketController : MonoBehaviour {
+
+    private Vector3 startPosition;
+    private enum Status { ready, fired, hit }
+    private Status status;
+
+    public Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+        startPosition = transform.localPosition;
+        status = Status.ready;
+        body = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    bool Fire(Vector3 direction)
+    {
+        if (status == Status.ready)
+        {
+            status = Status.fired;
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+}
